9418ecd1a9fe0726399e77517bb7cb7439683e4c
Kostya <kostya>
2016-10-09 01:20:02 +0300

    course 5 module 3 solution

---
+++ course5_module3_solution/scripts/app.js
@@ -0,0 +1,80 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective(){
+  var ddo = {
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: NarrowItDownController,
+    bindToController: true,
+    controllerAs: 'ctrl',
+    templateUrl: 'menuList.html'
+  };
+
+  return ddo;
+}
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var ctrl = this;
+  ctrl.searchTerm = "";
+  ctrl.noFound = false;
+
+  ctrl.getMenuItems = function(){
+    var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+    promise.then(
+      function (response){
+
+        // empty string and empty items array checking
+        if (ctrl.searchTerm === "" || response === undefined || (1 > response.length)) {
+          ctrl.noFound = true;
+          ctrl.found = [];
+        } else {
+          ctrl.found = response;
+          ctrl.noFound = false;
+        }
+
+      }
+    );
+  };
+
+  ctrl.removeItem = function(index) {
+    ctrl.found.splice(index, 1);
+  };
+
+}
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService ($http) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+        method: "GET",
+        url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+      })
+      .then(
+        function(response) {
+           var foundItems = [];
+           var menuItems = response.data.menu_items;
+
+           for (var i = 0; i < menuItems.length; i++) {
+             if (menuItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1){
+               foundItems.push(menuItems[i]);
+             }
+           }
+           return foundItems;
+         }
+       );
+  };
+
+}
+
+})();
